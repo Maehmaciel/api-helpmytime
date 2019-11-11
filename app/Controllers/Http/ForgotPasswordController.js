1281117f4b654dad2731dee1d64ed0f807591142
Maria E <mariaeugeniamaciel@gmail.com>
2019-11-10 20:31:03 -0400

    send forgot password email

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,14 +2,25 @@
 
 const crypto = use('crypto')
 const User = use('App/Models/User')
-
+const Mail = use('Mail')
 class ForgotPasswordController {
     async store({ request, response }){
     try {
         
             const email = request.input('email')
             const user = await User.findByOrFail('email', email)
-            const token = crypto.randonBytes(10).toString('hex')
+            const token = crypto.randomBytes(10).toString('hex')
+            await Mail.send(
+                //resources/views/emails
+                ['passwords.forgot', 'passwords.forgot_txt'],
+                { token, url: `https://www.google.com.br?token=${token}` },
+                message=>{
+                    message.
+                    to(email).
+                    from('mariaeugeniamaciel@gmail.com', 'Help my time').
+                    subject('email validate')
+                }
+            )
     }
     catch (erro) {
         return response.status(erro.status).send({ error : 'Houston, we have a problem'})
