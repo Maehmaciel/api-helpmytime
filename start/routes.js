a8c76260a7ff3c7b83489a61821ac0b659a71a06
Maria E <mariaeugeniamaciel@gmail.com>
2020-01-15 16:56:05 +0000

    routes

--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,25 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+
+Route.post('users', 'UserController.store')
+
+Route.post('login', 'LoginController.store')
+
+Route.post('forgotPassword', 'ForgotPasswordController.store')
+
+Route.group(()=>{
+//Route.resource('contact', 'UserContactController').apiOnly()
+Route.get('users', 'UserController.show')
+Route.resource('establishment', 'EstablishmentController').apiOnly()
+//Route.resource('establishment.address', 'AddressController').apiOnly()
+Route.resource('feature', 'FeatureController').apiOnly()
+//Route.resource('establishment.contact', 'EstablishmentContactController').apiOnly()
+Route.resource('service', 'ServiceController').apiOnly()
+Route.resource('category', 'CategoryController').apiOnly()
+Route.resource('schedule', 'ScheduleController').apiOnly()
+
+}).middleware(['auth'])
+
+
